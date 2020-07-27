2f0f300a9265e825414fde69255fa1626f299d77
Jong-ku Lee <addy94@naver.com>
2020-07-27 17:39:31 +0900

    setup boilerplate for express server

---
+++ server/src/config/express.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import methodOverride from 'method-override';
+import morgan from 'morgan';
+import compression from 'compression';
+import helmet from 'helmet';
+import cors from 'cors';
+import logger from './logger';
+import { logs } from './consts';
+import errorHandler from '../exception/error-handler';
+
+const app = express();
+app.use(morgan(logs, { stream: logger }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride());
+app.use(cors());
+app.use(helmet());
+app.use(compression());
+app.use(errorHandler);
+
+export default app;
