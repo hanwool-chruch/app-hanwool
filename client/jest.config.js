c5aaf6792c74613248a282e5bf53e43d0ee54b66
Atobaum <atobaum@gmail.com>
2020-07-27 17:21:11 +0900

    Move jest config in package.json into jest.config.js

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,5 +1,14 @@
 module.exports = {
-  moduleNameMapper: {
-    "\\.(css|less|sass|scss)$": "<rootDir>/src/lib/__mocks__/styleMock.js",
-  },
+	moduleNameMapper: {
+		'\\.(css|less|sass|scss)$': '<rootDir>/src/lib/__mocks__/styleMock.js',
+	},
+	transform: {
+		'^.+\\.ts$': 'ts-jest',
+	},
+	moduleFileExtensions: ['ts', 'js'],
+	globals: {
+		'ts-jest': {
+			diagnostics: true,
+		},
+	},
 };
