946c4dfd46b9aa3fccac34f6584368ef6b7f2fcb
Jong-ku Lee <addy94@naver.com>
2020-07-29 16:08:38 +0900

    Update service-api.ts

--- client/src/api/service-api.ts
+++ client/src/api/service-api.ts
@@ -1,7 +1,7 @@
 import { GET, POST, PUT, PATCH } from './utils';
 
-const create = async (data: JSON) => await POST('/api/service', data);
-const findAll = async (data: JSON) => await GET('/api/service', data);
-const softDelete = async (data: JSON) => await PATCH(`/api/service/remove`, data);
+const create = (data: JSON) => POST('/api/service', data);
+const findAll = (data: JSON) => GET('/api/service', data);
+const softDelete = (data: JSON) => PATCH(`/api/service/remove`, data);
 
 export { create, findAll, softDelete };
