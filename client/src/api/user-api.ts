d153c294fcc0023d457eff5a1aa0fe01972d2a85
Jong-ku Lee <addy94@naver.com>
2020-07-29 16:09:00 +0900

    Update user-api.ts

--- client/src/api/user-api.ts
+++ client/src/api/user-api.ts
@@ -1,8 +1,8 @@
 import { POST, GET } from './utils';
 
-const emailSignUp = async (data: JSON) => await POST('/api/user', data);
-const emailLogin = async (data: JSON) => await POST('/api/auth/email', data);
-const googleLogin = async (data: JSON) => await GET('/api/auth/google', data);
-const kakaoLogin = async (data: JSON) => await GET('/api/auth/kakao', data);
+const emailSignUp = (data: JSON) => POST('/api/user', data);
+const emailLogin = (data: JSON) => POST('/api/auth/email', data);
+const googleLogin = (data: JSON) => GET('/api/auth/google', data);
+const kakaoLogin = (data: JSON) => GET('/api/auth/kakao', data);
 
 export { emailLogin, emailSignUp, googleLogin, kakaoLogin };
