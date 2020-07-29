11f64d646612f787dfdd8728d9204ad4501e6a70
Atobaum <atobaum@gmail.com>
2020-07-29 16:57:11 +0900

    Add tab move animation

--- client/src/components/tab-selector/tab-selector.ts
+++ client/src/components/tab-selector/tab-selector.ts
@@ -6,7 +6,7 @@ export default class TabSelector extends Component {
 	private onMoveTab: (tab: string) => void;
 	constructor(onMoveTab: (tab: string) => void) {
 		super();
-		this.dom = document.createElement('ul');
+		this.dom = document.createElement('div');
 		this.dom.classList.add('tab-selector');
 		this.curTab = '';
 		this.onMoveTab = onMoveTab;
@@ -17,12 +17,19 @@ export default class TabSelector extends Component {
 		this.render();
 		this.dom?.addEventListener('click', (evt: any) => {
 			const tabName = evt.target.dataset.name;
-			if (tabName) this.onMoveTab(tabName);
+			if (!tabName) return;
+			this.dom?.querySelector('.tab-highlight');
+			this.onMoveTab(tabName);
+			const highlight: any = this.dom?.querySelector('.tab-highlight') as any;
+			const index = this.tabs.indexOf(tabName);
+			highlight.style.transform = `translateX(${index * 5}rem)`;
 		});
 	}
 
 	render(): void {
 		this.dom!.innerHTML = `
+		<div class="tab-highlight"></div>
+		<ul>
 			${this.tabs
 				.map(
 					(tab) => `
@@ -30,6 +37,7 @@ export default class TabSelector extends Component {
 				`
 				)
 				.join('')}
-        `;
+		</ul>
+		`;
 	}
 }
