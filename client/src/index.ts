cb4ad89cbf0c7e72a403abfad800e073dcfd6dd0
Atobaum <atobaum@gmail.com>
2020-07-30 00:00:21 +0900

    Add test code in index.ts

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,6 +1,7 @@
 import Header from './components/header';
 import MainPanel from './components/main-panel';
 import './global.scss';
+import PieChart from './components/content/statistics-content/pie-chart';
 class App {
 	private container: HTMLElement;
 	constructor(contianer: HTMLElement) {
@@ -11,6 +12,14 @@ class App {
 	init() {
 		this.container.appendChild(new Header().getDom());
 		this.container.appendChild(new MainPanel().getDom());
+		this.container.appendChild(
+			new PieChart([
+				{ color: 'yellow', name: '1', weight: 9 },
+				{ color: 'red', name: '2', weight: 3 },
+				{ color: 'green', name: '3', weight: 1 },
+				{ color: 'purple', name: '4', weight: 2 },
+			]).getDom()
+		);
 	}
 }
 
