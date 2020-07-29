6dc70638b8f3be2c807c81aedf1a6c9afb4bdb19
Jong-ku Lee <addy94@naver.com>
2020-07-29 21:42:07 +0900

    split webpack config to dev and prod 압축 관련 플러그인을 product에서만 진행하게 나눔 dev에서 컴파일 시간을 줄임

--- client/webpack.config.common.js
+++ client/webpack.config.common.js
@@ -1,8 +1,4 @@
 const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
 	entry: './src/index.ts',
@@ -13,44 +9,6 @@ module.exports = {
 	resolve: {
 		extensions: ['.ts', '.js', '.json'],
 	},
-	module: {
-		rules: [
-			{
-				test: /\.ts$/,
-				include: [path.resolve(__dirname, 'src')],
-				exclude: /node_modules/,
-				loader: 'ts-loader',
-			},
-			{
-				test: /\.js$/,
-				use: 'babel-loader',
-				exclude: /node_modules/,
-			},
-			{
-				test: /\.css$/,
-				use: [MiniCssExtractPlugin.loader, 'css-loader'],
-			},
-			{
-				test: /\.s[ac]ss$/,
-				use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-			},
-		],
-	},
-	plugins: [
-		new CleanWebpackPlugin({
-			cleanAfterEveryBuildPatterns: ['dist'],
-		}),
-		new MiniCssExtractPlugin({
-			filename: 'style.css',
-		}),
-		new HtmlWebpackPlugin({
-			template: path.resolve(__dirname, './public/index.html'),
-			filename: path.resolve(__dirname, './dist/index.html'),
-			inject: true, // inject built script in the end of body tag
-			alwaysWriteToDisk: true,
-		}),
-		new HtmlWebpackHarddiskPlugin(),
-	],
 	devServer: {
 		contentBase: path.join(__dirname, './dist'),
 		publicPath: '/static/',
--- client/webpack.config.dev.js
+++ client/webpack.config.dev.js
@@ -1,7 +1,39 @@
-const commonConfig = require("./webpack.config.common");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const commonConfig = require('./webpack.config.common');
 
 module.exports = {
-  ...commonConfig,
-  devtool: "source-map",
-  mode: "development",
+	...commonConfig,
+	devtool: 'source-map',
+	mode: 'development',
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				include: [path.resolve(__dirname, 'src')],
+				exclude: /node_modules/,
+				loader: 'ts-loader',
+			},
+			{
+				test: /\.js$/,
+				use: 'babel-loader',
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.css$/,
+				use: ['css-loader'],
+			},
+			{
+				test: /\.s[ac]ss$/,
+				use: ['css-loader', 'sass-loader'],
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, './public/index.html'),
+			filename: path.resolve(__dirname, './dist/index.html'),
+			inject: true, // inject built script in the end of body tag
+			alwaysWriteToDisk: true,
+		}),
+	],
 };
