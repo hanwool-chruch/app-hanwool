958b7eaacacf8d4a921af2ab6ae1842b35cc317d
Jong-ku Lee <addy94@naver.com>
2020-07-29 16:07:08 +0900

    Update category-api.ts

--- client/src/api/category-api.ts
+++ client/src/api/category-api.ts
@@ -1,8 +1,8 @@
 import { GET, POST, PUT, PATCH } from './utils';
 
-const create = async (data: JSON) => await POST('/api/category', data);
-const findAll = async (data: JSON) => await GET('/api/category', data);
-const update = async (data: JSON) => await PUT('/api/category', data);
-const softDelete = async (data: JSON) => await PATCH(`/api/category/remove`, data);
+const create = (data: JSON) => POST('/api/category', data);
+const findAll = (data: JSON) => GET('/api/category', data);
+const update = (data: JSON) => PUT('/api/category', data);
+const softDelete = (data: JSON) => PATCH(`/api/category/remove`, data);
 
 export { create, findAll, update, softDelete };
