c449d26ad628bc41864638e1090494bb78590fce
Kim Hyunyoung <atobaum@gmail.com>
2020-07-30 17:03:23 +0900

    Update webpack.config.dev.js

--- client/webpack.config.dev.js
+++ client/webpack.config.dev.js
@@ -21,4 +21,35 @@ module.exports = {
 	},
 	devtool: 'source-map',
 	mode: 'development',
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				include: [path.resolve(__dirname, 'src')],
+				exclude: /node_modules/,
+				loader: 'ts-loader',
+			},
+			{
+				test: /\.js$/,
+				use: 'babel-loader',
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.s[ac]ss$/,
+				use: ['style-loader', 'css-loader', 'sass-loader'],
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, './public/index.html'),
+			filename: path.resolve(__dirname, '../server/public/index.html'),
+			inject: true, // inject built script in the end of body tag
+			alwaysWriteToDisk: true,
+		}),
+	]
 };
