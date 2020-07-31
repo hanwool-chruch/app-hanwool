af49bf1f0acf0a63603b77e1059647450025bea6
Atobaum <atobaum@gmail.com>
2020-07-31 15:45:35 +0900

    Use history model inspitep of mocked data in MainPanel

--- client/src/components/main-panel/main-panel.ts
+++ client/src/components/main-panel/main-panel.ts
@@ -1,48 +1,15 @@
 import HistoryContent from '../content/history-content';
 import Component from '../component';
-import { History } from '@shared/dto/history-dto';
 import { AbstractContent } from '../content/abstract-content';
 import MonthSelector from '../month-selector';
 import TabSelector from '../tab-selector';
+import HistoryModel from '../../models/history-model';
 
-//날짜로 오름차순
-const testHistory: History[] = [
-	{
-		category: '밥',
-		historyDate: new Date('2020-08-01'),
-		content: '순댓국',
-		id: 1,
-		payment: '외상',
-		price: -4000,
-	},
-	{
-		category: '주식',
-		historyDate: new Date('2020-08-01'),
-		content: '상장폐지',
-		id: 2,
-		payment: '주식',
-		price: -5000,
-	},
-	{
-		category: '밥',
-		historyDate: new Date('2020-08-01'),
-		content: '순댓국1',
-		id: 3,
-		payment: '외상',
-		price: -3000,
-	},
-	{
-		category: '일',
-		historyDate: new Date('2020-08-03'),
-		content: '부업',
-		id: 4,
-		payment: '현금',
-		price: 6000,
-	},
-];
 export default class MainPanel extends Component {
 	dom: HTMLElement;
 	contents: Array<AbstractContent> = [];
+	//TODO resolve dependency
+	historyModel?: HistoryModel;
 
 	constructor() {
 		super();
@@ -52,6 +19,14 @@ export default class MainPanel extends Component {
 	}
 
 	init() {
+		// TODO: resolve dependency
+		// first parameter of conttructor of HistoryModel is service id
+		// default servide id is 1, for now.
+		this.historyModel = new HistoryModel(1);
+		this.historyModel.subscribe((data) => {
+			this.contents[0].load(data);
+		});
+
 		let curMonth = 6;
 		const monthSelector = new MonthSelector(6, (diff: number) => {
 			curMonth += diff;
@@ -67,10 +42,5 @@ export default class MainPanel extends Component {
 		this.getDom().appendChild(tabSelector.getDom());
 		this.contents.push(new HistoryContent());
 		this.dom.appendChild(this.contents[0].getDom());
-		/**
-		 * todo
-		 * 달력 content, 통계 content appendChild
-		 */
-		this.contents[0].load(testHistory);
 	}
 }
