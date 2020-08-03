ae160bf1538c75df5d539425529ae87fe65fbe9d
Atobaum <atobaum@gmail.com>
2020-08-03 17:04:58 +0900

    chore: Change conti-deploy filename

--- conti-deploy.sample.sh
+++ conti-deploy.sample.sh
@@ -1,13 +1,12 @@
-if [ -z $BRANCH ]; then
-	BRANCH="master"
-else
-	BRANCH=$BRANCH
-fi
+# Change file name and set branch manualy
+BRANCH="master"
 
 git fetch
+git checkout "$BRANCH"
 LOCAL=`git rev-parse "$BRANCH"`
 REMOTE=`git rev-parse origin/"$BRANCH"`
 
 if [ "$LOCAL" != "$REMOTE" ]; then
+	git pull
 	./build.sh
 fi
