d7272914c21b4390764f5062139c91be8fb83dda
Atobaum <atobaum@gmail.com>
2020-08-03 15:06:43 +0900

    feat: Basic calendar styling
    일요일, 이번달 아닌 날, 오늘 표시

--- client/src/components/content/calendar-content/calender-content.ts
+++ client/src/components/content/calendar-content/calender-content.ts
@@ -1,8 +1,9 @@
 import { AbstractContent } from '../abstract-content';
 import { History } from '@shared/dto/history-dto';
+import { groupByDate, calcTotal } from '../../../utils';
 
 export default class CalendarContent extends AbstractContent {
-	data: History[] | null = null;
+	data: History[] = [];
 
 	constructor() {
 		super();
@@ -24,28 +25,96 @@ export default class CalendarContent extends AbstractContent {
 			</div>
 		</div>
 		<div id="calendar-grid">
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
 		</div>
 		`;
+		this.render();
+	}
+
+	private render() {
+		const grid = this.dom!.querySelector('#calendar-grid') as HTMLDivElement;
+		grid.innerHTML = `
+		<div class="red">일</div>
+		<div>월</div>
+		<div>화</div>
+		<div>수</div>
+		<div>목</div>
+		<div>금</div>
+		<div>토</div>
+		`;
+
+		// What if there's no history in this month?
+		// Representitive date of the month
+		const refDate = this.data!.length > 0 ? this.data![0].historyDate : (new Date() as Date);
+
+		const firstDateOfMonth = new Date(refDate.getFullYear(), refDate.getMonth(), 1);
+		const lastDateOfMonth = new Date(refDate.getFullYear(), refDate.getMonth() + 1, 0);
+
+		const iterDate = new Date(firstDateOfMonth.getTime());
+		iterDate.setDate(iterDate.getDate() - iterDate.getDay());
+		while (iterDate.getDate() !== 1) {
+			grid.innerHTML += createCalendarItem(iterDate, true);
+			iterDate.setDate(iterDate.getDate() + 1);
+		}
+		while (iterDate.getMonth() === lastDateOfMonth.getMonth()) {
+			grid.innerHTML += createCalendarItem(iterDate, false);
+			iterDate.setDate(iterDate.getDate() + 1);
+		}
+		while (iterDate.getDay() !== 0) {
+			grid.innerHTML += createCalendarItem(iterDate, true);
+			iterDate.setDate(iterDate.getDate() + 1);
+		}
 	}
 
 	load(histories: History[]): void {
 		if (this.data === histories) return;
 
 		this.data = histories;
+		this.render();
+
+		// 이번달의 결제기록을 날짜별로 묶은 후 그 날의 수입/지출 데이터 추가
+		const dateDate = groupByDate(this.data).map((dayData) => ({
+			...dayData,
+			...calcTotal(dayData.dailyHistory),
+		}));
+		dateDate.forEach((data) => {
+			const calItem = this.dom!.querySelector(
+				'#' + createIdOfDay(data.dailyHistory[0].historyDate)
+			);
+			calItem!.innerHTML += `
+			<div class="cal-item-price">
+				<div class="earned">${data.earned > 0 ? `+ ${data.earned}` : ''}</div>
+				<div class="spent">${data.spent > 0 ? `- ${data.spent}` : ''}</div>
+			</div>
+			`;
+		});
 	}
 }
 
-function getLastDate(date: Date) {
-	return new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+function createCalendarItem(date: Date, gray: boolean) {
+	let className = '';
+	if (gray) className = 'gray';
+	else if (isToday(date)) className = 'today';
+	else if (isSunday(date)) className = 'red';
+	return `<div id="${createIdOfDay(date)}" class="cal-item ${className}">
+		<div class="cal-date-div">
+		${date.getDate()}
+		</div>
+	</div>`;
+}
+
+function isToday(date: Date) {
+	const today = new Date();
+	return (
+		date.getFullYear() === today.getFullYear() &&
+		date.getMonth() === today.getMonth() &&
+		date.getDate() === today.getDate()
+	);
+}
+
+function isSunday(date: Date) {
+	return date.getDay() === 0;
+}
+
+function createIdOfDay(date: Date) {
+	return `cal-item-${date.getMonth() + 1}-${date.getDate()}`;
 }
