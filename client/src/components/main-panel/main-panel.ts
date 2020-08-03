e8001a9069bcbfa0511000fb8a1bf32d62415c7a
Jong-ku Lee <addy94@naver.com>
2020-08-03 18:35:13 +0900

    feat:클라이언트 사이드 라우팅 구현 #14 - 주소창 입력시 push state, 그리고 화면 데이터 및 뷰 초기화 - popstate 시 url에 해당하는 month selector, tab selector, view 설정

--- client/src/components/main-panel/main-panel.ts
+++ client/src/components/main-panel/main-panel.ts
@@ -1,76 +1,115 @@
+import Router, { View } from '../../router';
 import HistoryContent from '../content/history-content';
 import Component from '../component';
-import { History } from '../content/abstract-content';
 import { AbstractContent } from '../content/abstract-content';
 import MonthSelector from '../month-selector';
 import TabSelector from '../tab-selector';
 
-//날짜로 오름차순
-const testHistory: History[] = [
-	{
-		category: '밥',
-		historyDate: new Date('2020-08-01'),
-		content: '순댓국',
-		id: 1,
-		payment: '외상',
-		price: -4000,
-	},
-	{
-		category: '주식',
-		historyDate: new Date('2020-08-01'),
-		content: '상장폐지',
-		id: 2,
-		payment: '주식',
-		price: -5000,
-	},
-	{
-		category: '밥',
-		historyDate: new Date('2020-08-01'),
-		content: '순댓국1',
-		id: 3,
-		payment: '외상',
-		price: -3000,
-	},
-	{
-		category: '일',
-		historyDate: new Date('2020-08-03'),
-		content: '부업',
-		id: 4,
-		payment: '현금',
-		price: 6000,
-	},
-];
-export default class MainPanel extends Component {
+class MainPanel extends Component {
 	dom: HTMLElement;
-	contents: Array<AbstractContent> = [];
+	monthSelector: any | null;
+	tabSelector: any | null;
+	contents: Map<String, AbstractContent>;
+	router: any;
 
 	constructor() {
 		super();
 		this.dom = document.createElement('main');
+		this.contents = new Map<String, AbstractContent>();
 		this.dom.classList.add('main-panel');
+		this.monthSelector = null;
+		this.tabSelector = null;
+		this.router = null;
 		this.init();
+		this.listener();
 	}
 
 	init() {
-		let curMonth = 6;
-		const monthSelector = new MonthSelector(6, (diff: number) => {
+		const views = this.initViews();
+		this.router = new Router(views);
+
+		let curMonth = parseInt(this.router.getCurrentYearAndMonth().split('-')[1]);
+		this.monthSelector = new MonthSelector(curMonth, (diff: number) => {
 			curMonth += diff;
+			this.router.loadHistoryData(`2020-${curMonth}`);
+			this.router.updateCurrentUrl();
 			return curMonth;
 		});
 
-		const tabSelector = new TabSelector((tab) => {
+		this.tabSelector = new TabSelector((tab) => {
 			console.log(tab);
+
+			let page = '';
+			switch (tab) {
+				case '내역':
+					page = 'history';
+					break;
+				case '달력':
+					page = 'calendar';
+					break;
+				case '통계':
+					page = 'graph';
+					break;
+				default:
+					throw new Error('no tab exists');
+			}
+			this.router.loadView(page);
+			this.router.updateCurrentUrl();
 			alert(`Move to ${tab} tab`);
 		});
 
-		this.getDom().appendChild(monthSelector.getDom());
-		this.getDom().appendChild(tabSelector.getDom());
-		this.contents.push(new HistoryContent());
-		this.dom.appendChild(this.contents[0].getDom());
-		/**
-		 * todo
-		 * 달력 content, 통계 content appendChild
-		 */
-		this.contents[0].load(testHistory);
+		this.getDom().appendChild(this.monthSelector.getDom());
+		this.getDom().appendChild(this.tabSelector.getDom());
+	}
+
+	private initViews(): Array<View> {
+		const views = [] as Array<View>;
+		const histroyView = { viewName: 'history', component: new HistoryContent() };
+		const calendarView = { viewName: 'calendar', component: new HistoryContent() };
+		const graphView = { viewName: 'graph', component: new HistoryContent() };
+
+		views.push(histroyView);
+		views.push(calendarView);
+		views.push(graphView);
+
+		return views;
+	}
+
+	private listener() {
+		window.addEventListener('popstate', this.popStateHandler.bind(this));
+	}
+
+	/**
+	 * TODO
+	 * router 내로 옮기고 tabSelector와 monthSelector가 router를 구독하게?
+	 */
+	private async popStateHandler() {
+		const routeArr = location.pathname.replace('/', '').split('/');
+		const serviceId = parseInt(routeArr[0], 16) - 3000;
+		const yearAndMonth = routeArr[1];
+		const page = routeArr[2];
+		this.router.setCurrent({ serviceId, yearAndMonth, page });
+		console.info('popstate', { serviceId, yearAndMonth, page });
+
+		let tabName;
+		switch (page) {
+			case 'history':
+				tabName = '내역';
+				break;
+			case 'calendar':
+				tabName = '달력';
+				break;
+			case 'graph':
+				tabName = '통계';
+				break;
+			default:
+				throw new Error('no pageName for tab');
+		}
+
+		this.monthSelector?.setMonth(yearAndMonth.split('-')[1]);
+		this.tabSelector.setHighlight(tabName);
+		this.router.loadView(page);
 	}
 }
+
+export default MainPanel;
---
+++ client/src/router.ts
@@ -0,0 +1,152 @@
+import { AbstractContent } from './components/content/abstract-content';
+import NotFoundPage from './pages/not-found';
+import { History } from './components/content/abstract-content';
+
+const testHistory: History[] = [
+	{
+		category: '밥',
+		historyDate: new Date('2020-08-01'),
+		content: '순댓국',
+		id: 1,
+		payment: '외상',
+		price: -4000,
+	},
+	{
+		category: '주식',
+		historyDate: new Date('2020-08-01'),
+		content: '상장폐지',
+		id: 2,
+		payment: '주식',
+		price: -5000,
+	},
+	{
+		category: '밥',
+		historyDate: new Date('2020-08-01'),
+		content: '순댓국1',
+		id: 3,
+		payment: '외상',
+		price: -3000,
+	},
+	{
+		category: '일',
+		historyDate: new Date('2020-08-03'),
+		content: '부업',
+		id: 4,
+		payment: '현금',
+		price: 6000,
+	},
+];
+
+interface View {
+	viewName: string;
+	component: AbstractContent;
+}
+
+interface CurrentData {
+	serviceId: number;
+	yearAndMonth: string;
+	page: string;
+}
+
+class Router {
+	private views: Map<String, AbstractContent>;
+	private root: string;
+	private current: CurrentData;
+
+	constructor(pages: Array<View>) {
+		this.views = new Map<String, AbstractContent>();
+		this.root = '/';
+		const now = new Date();
+		this.current = {
+			serviceId: 1,
+			yearAndMonth: now.getFullYear() + '-' + now.getMonth() + 1,
+			page: 'history',
+		};
+		this.init(pages);
+	}
+
+	private async init(pages: Array<View>) {
+		pages.forEach((page) => this.addView(page.viewName, page.component));
+
+		const routeArr = location.pathname.replace(this.root, '').split('/');
+		if (routeArr.length !== 3) {
+			this.loadView('not-found');
+			return;
+		}
+
+		const serviceId = parseInt(routeArr[0], 16) - 3000;
+		const yearAndMonth = routeArr[1];
+		const page = routeArr[2];
+
+		await this.loadHistoryData(yearAndMonth, serviceId);
+		this.loadView(page);
+		this.updateCurrentUrl();
+	}
+
+	private addView(viewName: string, component: AbstractContent): void {
+		this.views.set(viewName, component);
+	}
+
+	private getView(viewName: string): AbstractContent | null {
+		if (this.views.has(viewName)) {
+			return this.views.get(viewName) as AbstractContent;
+		} else {
+			return null;
+		}
+	}
+
+	public loadView(page: string) {
+		const mainPanel = document.querySelector('main') as HTMLElement;
+		const matchedView = this.getView(page);
+		if (matchedView) {
+			while (mainPanel.childElementCount !== 2) {
+				mainPanel.lastElementChild?.remove();
+			}
+			mainPanel.appendChild(matchedView.getDom());
+			this.current.page = page;
+		} else {
+			const app = document.querySelector('#app') as HTMLElement;
+			const url = `${this.root}not-found`;
+			app.innerHTML = NotFoundPage.innerHTML;
+			history.pushState({}, '', url);
+		}
+	}
+
+	public async loadHistoryData(
+		yearAndMonth: string = this.current.yearAndMonth,
+		serviceId: number = this.current.serviceId
+	) {
+		//todo 뷰의 데이터 전부 로드
+		this.views.get(this.current.page)!.load(testHistory);
+
+		this.current.serviceId = serviceId;
+		this.current.yearAndMonth = yearAndMonth;
+	}
+
+	public updateCurrentUrl() {
+		console.info(
+			'pushstate',
+			`${this.root}${(this.current.serviceId + 3000).toString(16)}/${this.current.yearAndMonth}/${
+				this.current.page
+			}`
+		);
+		history.pushState(
+			null,
+			'',
+			`${this.root}${(this.current.serviceId + 3000).toString(16)}/${this.current.yearAndMonth}/${
+				this.current.page
+			}`
+		);
+	}
+
+	public setCurrent(current: CurrentData) {
+		this.current = current;
+	}
+
+	public getCurrentYearAndMonth() {
+		return this.current.yearAndMonth;
+	}
+}
+
+export { View };
+export default Router;
