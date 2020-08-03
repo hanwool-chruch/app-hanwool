20648c4c2b57d2e832387f4c0b9d6aed3729575b
Atobaum <atobaum@gmail.com>
2020-08-03 16:57:04 +0900

    feat: Create build script
    build.sh
    - removes build files
    - terminates ans restarts server instance
    - build client files

--- build.sh
+++ build.sh
@@ -1,8 +1,13 @@
-echo "building"
-cd client
+cd server
+rm -rf build
+rm -rf public
+echo "terminating pm2 instances"
+npm run pm2 delete all
 npm i
-npm run build
+npm run pm2 install typescript
+npm run pm2 start ts-node -- ./src/main.ts
 
-cd ../server
+echo "building"
+cd ../client
 npm i
 npm run build
